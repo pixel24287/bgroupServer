8805275c1fe725368e3da75eb5acb867062ecd9e
pixel24287 <pixel24287@gmail.com>
2022-07-22 06:54:48 +0900

    Initial commit2

--- src/redis.js
+++
@@ -1,9 +0,0 @@
-// const redis = require('redis');
-
-// const redisClient = redis.createClient({
-//     url : `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-//     password : process.env.REDIS_PASSWORD
-// })
-// redisClient.on('error', (err) => console.log('Redis Client Error', err));
-
-// module.exports = redisClient
